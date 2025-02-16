7e88a85b2c4070c050bea267976adc8adc46b436
Agony <gcl1197134883@gmail.com>
2025-02-16 14:57:01 +0800

    项目初始化

---
+++ src/main/java/com/agony/picturebackend/common/PageRequest.java
@@ -0,0 +1,18 @@
+package com.agony.picturebackend.common;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @author: Agony
+ * @create: 2025/2/16 14:56
+ * @describe:
+ */
+
+@Data
+@Accessors(chain = true)
+public class PageRequest {
+
+    
+}
+
