10974cd43723da29b22b9a8ac8b1926e59724b9c
Agony <gcl1197134883@gmail.com>
2025-02-17 10:43:46 +0800

    项目初始化

--- src/main/java/com/agony/picturebackend/PictureBackendApplication.java
+++ src/main/java/com/agony/picturebackend/PictureBackendApplication.java
@@ -16,6 +16,10 @@ public class PictureBackendApplication {
 
         // todo 自定义异常 1. 错误代码(ErrorCode) 2. 响应包装类 3. 全局异常处理器 4. 请求包装类 5. 全局跨域配置
 
+
+        // todo User类可以加一个 @Accessors(chain = true) 注解
+
+        // todo 在写UserService的业务逻辑时，不要用 throw new BusinessException, 试着用 ThrowUtils.throwIf()
     }
 
 }
