3529a289d18752e7a1c55daa36d90e1d4645d187
Agony <gcl1197134883@gmail.com>
2025-02-17 19:03:28 +0800

    项目初始化

---
+++ src/main/java/com/agony/picturebackend/aop/AuthInterceptor.java
@@ -0,0 +1,12 @@
+package com.agony.picturebackend.aop;
+
+/**
+ * @author: Agony
+ * @create: 2025/2/17 18:32
+ * @describe:
+ */
+public class AuthInterceptor {
+
+
+    // todo 在写aop前面的时候调试一下 String mustRole = authCheck.mustRole()，看一下这个mustRole
+}
